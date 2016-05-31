f2fefb805e1423f37a3a44cd20827e22b647776d
Simon Liang <simon@divby0.io>
2016-05-31 18:32:19 +0800

    finished v1 breakout

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log("sup");
+function createRectangle(startX, startY, width, height) {
+  return new Phaser.Rectangle(startX, startY, width, height);
+}
+
+function displayRectangle(rect) {
+  game.debug.geom(rect, '#ffffff');
+}
+
+var game = new Phaser.Game(400, 450);
+game.state.add('main', window.gameState);
+game.state.start('main');
